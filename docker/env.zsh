3b817346d210af745a238dd544aa89b5556f88ca
Steven Berlanga <zabawaba99@gmail.com>
2015-07-17 08:50:39 -0400

    making sure docker is running, the environment is set and that there is a mysql container running

--- docker/env.zsh
+++
@@ -1,4 +0,0 @@
-if [ -z "$DOCKER_HOST" ]; then
-  eval $(boot2docker shellinit 2>/dev/null)
-  export DOCKER_IP=$(boot2docker ip 2> /dev/null)
-fi
--- docker/functions.zsh
+++ docker/functions.zsh
@@ -1,7 +1,5 @@
 docker_up(){
   boot2docker up
-  export DOCKER_IP=$(boot2docker ip 2> /dev/null)
-  eval $(boot2docker shellinit 2> /dev/null)
 }
 
 drop_the_mic(){
@@ -11,3 +9,17 @@ drop_the_mic(){
 bye_bye_dangling(){
   docker images -q --filter "dangling=true" | xargs -n1 docker rmi -f
 }
+
+if [ "$(boot2docker status)" != "running" ]; then
+  docker_up
+fi
+
+if [ -z "$DOCKER_HOST" ]; then
+  export DOCKER_IP=$(boot2docker ip 2> /dev/null)
+  eval $(boot2docker shellinit 2> /dev/null)
+fi
+
+if ! docker ps | grep -q mysql_container ; then
+  docker rm mysql_container > /dev/null 2>&1
+  docker run -d -e MYSQL_ROOT_PASSWORD=root -p '3307:3306' --name="mysql_container" mysql:5.6 1> /dev/null
+fi
