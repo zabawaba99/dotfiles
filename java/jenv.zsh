c64b5c56c782e2e92300b553e78870570921e846
Steven Berlanga <zabawaba99@gmail.com>
2015-09-02 09:06:11 -0400

    making sure that java home is set

--- java/jenv.zsh
+++ java/jenv.zsh
@@ -3,3 +3,15 @@ export JENV_ROOT=/usr/local/opt/jenv
 if which jenv > /dev/null; then
   eval "$(jenv init -)";
 fi
+
+export JAVA_HOME="$JENV_ROOT/versions/$(jenv version-name)"
+
+function global_java_version() {
+	jenv global $@
+	export JAVA_HOME="$JENV_ROOT/versions/$(jenv version-name)"
+}
+
+function local_java_version() {
+	jenv local $@
+	export JAVA_HOME="$JENV_ROOT/versions/$(jenv version-name)"
+}
