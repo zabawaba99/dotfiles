45f56a1657fe758fabb0dd4aca63ad2662c55ba4
Steven Berlanga <zabawaba99@gmail.com>
2016-02-19 19:11:42 -0500

    adding rabbit_mq container

--- docker/functions.zsh
+++ docker/functions.zsh
@@ -23,9 +23,21 @@ if [ -z "$DOCKER_HOST" ]; then
   eval $(docker-machine env dev)
 fi
 
-if ! docker ps | grep -q mysql_container ; then
-  docker rm mysql_container > /dev/null 2>&1
-  docker run -d -v /etc/my.cnf:/etc/mysql/conf.d -e MYSQL_ROOT_PASSWORD=root -p '3307:3306' --name="mysql_container" mysql:5.6.25 1> /dev/null
-fi
+start_mysql_container() {
+  if ! docker ps | grep -q mysql_container ; then
+    docker rm mysql_container > /dev/null 2>&1
+    docker run -d -v /etc/my.cnf:/etc/mysql/conf.d -e MYSQL_ROOT_PASSWORD=root -p '3307:3306' --name="mysql_container" mysql:5.6.25 1> /dev/null
+  fi
 
-export DATABASE_DSN="root:root@tcp($DOCKER_IP:3307)/"
+  export DATABASE_DSN="root:root@tcp($DOCKER_IP:3307)/"
+}
+start_mysql_container
+
+start_rabbit_container() {
+  if ! docker ps | grep -q rabbit_container ; then
+    docker rm rabbit_container > /dev/null 2>&1
+    docker run -d -hostname my-rabbit --name rabbit_container -p 15672:15672 -p 5672:5672 rabbitmq:3-management 1> /dev/null
+  fi
+
+  export RABBITMQ_URI="root:root@tcp($DOCKER_IP:3307)/"
+}
